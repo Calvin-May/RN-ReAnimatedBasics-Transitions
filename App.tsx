0ce806f59734218f7636874904f7e961d41ff123
Calvin May <calvinkylemay.dev@gmail.com>
2025-04-23 07:09:57 -0400

    Added Boilderplate Code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Transitions } from './screens/Transitions';
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Transitions />
     </GestureHandlerRootView>
   );
 }
