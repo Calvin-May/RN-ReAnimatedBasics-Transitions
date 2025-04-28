086f899de8b2dccb49ed1a13af46fad4f4dcd162
Calvin May <calvinkylemay.dev@gmail.com>
2025-04-28 08:35:20 -0400

    Added Spring animation during transition of card positions

--- components/AnimatedCard.tsx
+++ components/AnimatedCard.tsx
@@ -1,5 +1,9 @@
 import { StyleSheet, Dimensions } from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, {
+  DerivedValue,
+  interpolate,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 import type { Cards } from '../components';
 import { Card, StyleGuide } from '../components';
@@ -16,20 +20,40 @@ const styles = StyleSheet.create({
 });
 
 interface AnimatedCardProps {
-  toggled: boolean;
+  transition: DerivedValue<any>;
   index: number;
   card: Cards;
 }
 
-export const AnimatedCard = ({ card, toggled, index }: AnimatedCardProps) => {
-  const alpha = toggled ? ((index - 1) * Math.PI) / 6 : 0;
-  const style = {
-    transform: [
-      { translateX: origin },
-      { rotate: `${alpha}rad` },
-      { translateX: -origin },
-    ],
-  };
+export const AnimatedCard = ({
+  card,
+  transition,
+  index,
+}: AnimatedCardProps) => {
+  // If un-toggled, rotation is 0
+  //- If Toggled, card Index -1 * 180degrees / 6.... Index of 0 = rotation of -40degrees and so on
+  //const rotation = transition ? ((index - 1) * Math.PI) / 6 : 0;
+  const style = useAnimatedStyle(() => {
+    const rotate = interpolate(
+      transition.value,
+      [0, 1],
+      [0, ((index - 1) * Math.PI) / 6],
+    );
+    return {
+      transform: [
+        { translateX: origin },
+        { rotate: `${rotate}rad` },
+        { translateX: -origin },
+      ],
+    };
+  });
+  // const style  = {
+  //   transform: [
+  //     { translateX: origin },
+  //     { rotate: `${rotation}rad` },
+  //     { translateX: -origin },
+  //   ],
+  // };
   return (
     <Animated.View key={card} style={[styles.overlay, style]}>
       <Card {...{ card }} />
--- screens/Transitions.tsx
+++ screens/Transitions.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Button, StyleGuide, cards } from '../components';
 
 import { AnimatedCard } from '../components/AnimatedCard';
+import {
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,11 +19,26 @@ const styles = StyleSheet.create({
 });
 
 export const Transitions = () => {
-  const [toggled, setToggle] = useState(false);
+  const [toggled, setToggle] = useState<boolean>(false);
+  // Created Shared Animation value that depends on react State above
+  const isToggled = useSharedValue<number>(0);
+
+  // Side Effect on each render that only runs when toggled or isToggled changes
+  useEffect(() => {
+    // When we toggle the state, also set the animation value to play our animation
+    isToggled.value = toggled ? 1 : 0;
+  }, [toggled, isToggled]);
+
+  // Create a new animation value based on other animation values using useDerivedValue
+  //--Aka, our spring transition animation is dependent on the value of isToggled.
+  const transition = useDerivedValue(() => {
+    return withSpring(isToggled.value);
+  });
+
   return (
     <View style={styles.container}>
       {cards.slice(0, 3).map((card, index) => (
-        <AnimatedCard key={card} {...{ index, card, toggled }} />
+        <AnimatedCard key={card} {...{ index, card, transition }} />
       ))}
       <Button
         label={toggled ? 'Reset' : 'Start'}
