e019e5e7d16703cd3daccebf50cbbb1deb11cf68
Calvin May <calvinkylemay.dev@gmail.com>
2025-04-29 07:51:32 -0400

    Added Optional config parameter to useSpring hook to customzise animation during implementation

--- hooks/animation/useSpring.ts
+++ hooks/animation/useSpring.ts
@@ -4,8 +4,9 @@ import {
   useDerivedValue,
   withSpring,
 } from 'react-native-reanimated';
+import type { SpringConfig } from 'react-native-reanimated/lib/typescript/animation/springUtils';
 
-const useSpring = (state: number | boolean) => {
+const useSpring = (state: number | boolean, config?: SpringConfig) => {
   // Created Shared Animation value that depends on react State above
   const animationValue = useSharedValue<number>(0);
 
@@ -18,7 +19,7 @@ const useSpring = (state: number | boolean) => {
   // Create a new animation value based on other animation values using useDerivedValue
   //--Aka, our spring transition animation is dependent on the value of isToggled.
   const transition = useDerivedValue(() => {
-    return withSpring(animationValue.value);
+    return withSpring(animationValue.value, config);
   });
 
   return transition;
